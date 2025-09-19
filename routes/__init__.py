1b2d8e767661a9bf13194a0916668f7bfd222a34
d01k1 <107388803+d01ki@users.noreply.github.com>
2025-09-20 00:51:27 +0900

    Add routes package initialization

---
+++ routes/__init__.py
@@ -0,0 +1,9 @@
+"""
+ルーティングモジュール
+"""
+from .main_routes import main_bp
+from .practice_routes import practice_bp
+from .exam_routes import exam_bp
+from .admin_routes import admin_bp
+
+__all__ = ['main_bp', 'practice_bp', 'exam_bp', 'admin_bp']
