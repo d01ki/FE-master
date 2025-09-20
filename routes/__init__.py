d44c453b2d84b160102a8e680131cae9ef00d691
d01k1 <107388803+d01ki@users.noreply.github.com>
2025-09-20 19:38:23 +0900

    feat: Update routes init to include ranking blueprint

--- routes/__init__.py
+++ routes/__init__.py
@@ -5,5 +5,6 @@ from .main_routes import main_bp
 from .practice_routes import practice_bp
 from .exam_routes import exam_bp
 from .admin_routes import admin_bp
+from .ranking_routes import ranking_bp
 
-__all__ = ['main_bp', 'practice_bp', 'exam_bp', 'admin_bp']
+__all__ = ['main_bp', 'practice_bp', 'exam_bp', 'admin_bp', 'ranking_bp']
